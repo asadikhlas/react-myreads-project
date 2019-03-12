f166c9d4721d00f2c960d8096082a992e9c01741
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-12 22:58:06 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Views/home";
 import Search from "./Views/search";
+import Provider, { MyContext } from "./provider";
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        <Switch>
-          <Route exact path={"/"} component={Home} />
-          <Route exact path={"/search"} component={Search} />
-        </Switch>
+        <Provider>
+          <Switch>
+            <Route
+              exact
+              path={"/"}
+              render={() => (
+                <MyContext.Consumer>
+                  {context => <Home {...context} />}
+                </MyContext.Consumer>
+              )}
+            />
+            <Route exact path={"/search"} component={Search} />
+          </Switch>
+        </Provider>
       </div>
     );
   }
