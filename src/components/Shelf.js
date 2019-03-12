1c64464caef7e58f03bdfe7488521a3c9bd6db31
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-12 22:07:54 +0500

    commit

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import Book from './book';
 
 export default class Shelf extends Component {
   render() {
     return (
-      <div>
-        book
+        <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+       
+           <Book/>
+          </ol>
+        </div>
       </div>
     )
   }
