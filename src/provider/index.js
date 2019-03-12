83fefac2782accb82daa1e2ae29819a64695a9c0
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-12 22:49:19 +0500

    commit

---
+++ src/provider/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+export const MyContext = React.createContext();
+
+export default class Index extends Component {
+  constructor() {
+    super();
+    this.state = {
+        books:[],
+        currentlyReading:[],
+        wantToRead:[],
+        read:[]
+    };
+  }
+  render() {
+    return (<MyContext.Provider value={{...this.state}}>
+        {this.props.children}
+    </MyContext.Provider>)
+  }
+}
