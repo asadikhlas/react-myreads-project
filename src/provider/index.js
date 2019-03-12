0f1231a8931aa251b98fe374423fc57409dd275a
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-12 22:49:35 +0500

    commit

--- src/provider/index.js
+++ src/provider/index.js
@@ -5,15 +5,17 @@ export default class Index extends Component {
   constructor() {
     super();
     this.state = {
-        books:[],
-        currentlyReading:[],
-        wantToRead:[],
-        read:[]
+      books: [],
+      currentlyReading: [],
+      wantToRead: [],
+      read: []
     };
   }
   render() {
-    return (<MyContext.Provider value={{...this.state}}>
+    return (
+      <MyContext.Provider value={{ ...this.state }}>
         {this.props.children}
-    </MyContext.Provider>)
+      </MyContext.Provider>
+    );
   }
 }
