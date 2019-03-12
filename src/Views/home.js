6f1b598ffe0cc2d1be6e4aefaa7e42c646331d26
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-12 22:40:51 +0500

    commit

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,19 +1,30 @@
-import React, { Component } from 'react'
-import Shelf from '../components/Shelf';
-import Fab from '../components/Fab';
+import React, { Component } from "react";
+import Shelf from "../components/Shelf";
+import Fab from "../components/Fab";
+import {getAll} from '../BooksAPI';
 
 export default class Home extends Component {
+    async componentDidMount(){
+        try {
+            const books = await getAll();
+            console.log(books);
+        } catch (error) {
+            console.log(error)
+        }
+    }
   render() {
     return (
-        <div className="list-books">
+      <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <Shelf />
+          <Shelf title="Currently Reading" />
+          <Shelf title="Want To Read" />
+          <Shelf title="Read" />
         </div>
-        <Fab/>
+        <Fab />
       </div>
-    )
+    );
   }
 }
