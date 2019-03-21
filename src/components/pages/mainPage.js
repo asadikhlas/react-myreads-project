c1685c609c85fbd078b912a34bb35af8f3118ebf
Asad Ikhlas <asadikhlas03@gmail.com>
2019-03-21 19:26:00 +0500

    commit

--- src/components/pages/mainPage.js
+++ src/components/pages/mainPage.js
@@ -53,7 +53,7 @@ class MainPage extends Component {
             />
           </div>
         </div>
-        <div className="open-search">  
+        <div className="open-search">   
           <Link to="/search">Add a book</Link>
         </div>
       </div>
